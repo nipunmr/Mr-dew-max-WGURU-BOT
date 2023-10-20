3117881f4df78cc1e522cb6a7f99484f3da10c44
nipunmr <148508741+nipunmr@users.noreply.github.com>
2023-10-20 08:32:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94764220802', 'me', true],
+  ['94764220802', 'Mr dew max', true], 
   [''] 
 ] //Number of owners
 
